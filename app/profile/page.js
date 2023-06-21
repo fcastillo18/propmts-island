441f2bd302f55a69f869b603206da12a57208972
Franklin Castillo <franklincastillo18@gmail.com>
2023-06-21 11:47:31 -0400

    feat: adding edit and delete capabilites to app

--- app/profile/page.js
+++ app/profile/page.js
@@ -8,9 +8,26 @@ import Profile from '@components/profile'
 const MyProfile = () => {
   const { data: session } = useSession()
   const [posts, setPosts] = useState([])
+  const router = useRouter();
 
-  const handleEdit = () => {}
-  const handleDelete = async () => {}
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+  }
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(`Are you sure you want to delete this post?`)
+    if(hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+        const filteredPosts = posts.filter(p => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
 
    useEffect(() => {
     const fetchData = async () => {
